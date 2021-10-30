f6b95f5887ed315e77ffa43577141a450a7c9291
putri shafira <pudsfira@gmail.com>
2021-10-30 22:30:15 +0700

    project pertama

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+function openPage() {
+    var x = document.getElementById("search").value;
+
+    if (x === "start up") {
+        window.open(href="startup.html");
+    }
+
+    else if (x === "ponyo") {
+        window.open(href="ponyo.html");
+    }
+
+    else if (x === "squid game") {
+        window.open(href="Squid%20Game.html");
+    }
+
+    else if (x === "weathering with you") {
+        window.open(href="Tenki%20no%20Ko.html");
+    }
+
+    else if (x === "tenki no ko") {
+        window.open(href="Tenki%20no%20Ko.html");
+    }
+
+    else {
+        window.open(href="belum.html");
+    }
+}
